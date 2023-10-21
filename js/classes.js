048843e357cfb93440f3c5364b2a9ad28f1e4f6d
Mo <145007283+Momo-010@users.noreply.github.com>
2023-10-21 22:35:50 +0100

    Removed initial draw function from fighter class and replaced with Sprite class draw function
    Extended sprite class to fighter class and Used 'super' operator to inherit methods from sprite class

--- js/classes.js
+++ js/classes.js
@@ -1,105 +1,110 @@
 class Sprite {
-    constructor({ position, imageSrc, scale = 1 , framesMax=1}) {
-        this.position = position;
-        this.height = 150;
-        this.width = 50;
-        this.image = new Image()
-        this.image.src = imageSrc;
-        this.scale = scale;
-        this.framesMax = framesMax;
+  constructor({ position, imageSrc, scale = 1, framesMax = 1 }) {
+    this.position = position;
+    this.height = 150;
+    this.width = 50;
+    this.image = new Image();
+    this.image.src = imageSrc;
+    this.scale = scale;
+    this.framesMax = framesMax;
+    this.framesCurrent = 0;
+    this.framesElapsed = 0;
+    this.framesHold = 5;
+  }
+
+  draw() {
+    c.drawImage(
+      this.image,
+      this.framesCurrent * (this.image.width / this.framesMax),
+      0,
+      this.image.width / this.framesMax,
+      this.image.height,
+      this.position.x,
+      this.position.y,
+      (this.image.width / this.framesMax) * this.scale,
+      this.image.height * this.scale
+    );
+  }
+
+  update() {
+    this.draw();
+    this.framesElapsed++;
+    if (this.framesElapsed % this.framesHold === 0) {
+      if (this.framesCurrent < this.framesMax - 1) {
+        this.framesCurrent++;
+      } else {
         this.framesCurrent = 0;
-        this.framesElapsed = 0;
-        this.framesHold = 5;
+      }
     }
+  }
 
-    draw() {
-        c.drawImage(
-          this.image,
-          this.framesCurrent*(this.image.width/this.framesMax),
-          0,
-          this.image.width/this.framesMax,
-          this.image.height,
-          this.position.x,
-          this.position.y,
-          (this.image.width/this.framesMax) * this.scale,
-          this.image.height * this.scale
-        );
-    }
-
-    update() {
-        this.draw();
-        this.framesElapsed++;
-        if (this.framesElapsed % this.framesHold === 0) {
-            if(this.framesCurrent < this.framesMax -1){
-            this.framesCurrent++;}
-            else{
-                this.framesCurrent = 0;
-            }
-        }
-    }
-
-    atttack() {
-        this.isAttacking = true;
-        setTimeout(() => {
-            this.isAttacking = false;
-        }, 100);
-    }
+  atttack() {
+    this.isAttacking = true;
+    setTimeout(() => {
+      this.isAttacking = false;
+    }, 100);
+  }
 }
 
-class Fighter {
-    constructor({ position, velocity, color = 'red', offset }) {
-        this.position = position;
-        this.velocity = velocity;
-        this.height = 150;
-        this.width = 50;
-        this.lastKey;
-        this.attackBox = {
-            position: {
-                x: this.position.x,
-                y: this.position.y
-            },
-            offset, //offset: offset, 
+class Fighter extends Sprite {
+  constructor({
+    position,
+    velocity,
+    color = "red",
+    offset,
+    imageSrc,
+    scale = 1,
+    framesMax = 1,
+  }) {
+    super({
+        position,
+        image,
+        scale,
+        framesMax,
+        framesCurrent,
+        framesElapsed,
+        framesHold
+    });
 
-            width: 100,
-            height: 50
-        }
-        this.color = color;
-        this.isAttacking;
-        this.health = 100;
+    this.velocity = velocity;
+    this.height = 150;
+    this.width = 50;
+    this.lastKey;
+    this.attackBox = {
+      position: {
+        x: this.position.x,
+        y: this.position.y,
+      },
+      offset, //offset: offset,
+
+      width: 100,
+      height: 50,
+    };
+    this.color = color;
+    this.isAttacking;
+    this.health = 100;
+  }
+
+  update() {
+    this.draw();
+    this.attackBox.position.x = this.position.x + this.attackBox.offset.x;
+    this.attackBox.position.y = this.position.y;
+
+    this.position.x += this.velocity.x;
+
+    this.position.y += this.velocity.y;
+
+    if (this.position.y + this.height + this.velocity.y >= canvas.height - 97) {
+      this.velocity.y = 0;
+    } else {
+      this.velocity.y += gravity;
     }
+  }
 
-    draw() {
-        c.fillStyle = this.color;
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
-
-        //Attack Box
-        if (this.isAttacking) {
-            c.fillStyle = 'green'
-            c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height)
-        }
-    }
-
-    update() {
-        this.draw();
-        this.attackBox.position.x = this.position.x + this.attackBox.offset.x
-        this.attackBox.position.y = this.position.y
-
-        this.position.x += this.velocity.x
-
-        this.position.y += this.velocity.y
-
-        if (this.position.y + this.height + this.velocity.y >= canvas.height - 97) {
-            this.velocity.y = 0;
-        }
-        else {
-            this.velocity.y += gravity;
-        }
-    }
-
-    atttack() {
-        this.isAttacking = true;
-        setTimeout(() => {
-            this.isAttacking = false;
-        }, 100);
-    }
+  atttack() {
+    this.isAttacking = true;
+    setTimeout(() => {
+      this.isAttacking = false;
+    }, 100);
+  }
 }
